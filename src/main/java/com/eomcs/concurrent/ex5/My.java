9dba4206a69819a1c4d4079da3e316573b987448
sunwoopark27 <sun960127@naver.com>
2021-03-16 14:05:29 +0900

    .'

--- src/main/java/com/eomcs/concurrent/ex5/My.java
+++ src/main/java/com/eomcs/concurrent/ex5/My.java
@@ -1,17 +1,12 @@
-// 멀티 스레딩(비동기 프로그래밍)의 문제점 - 사례 1
 package com.eomcs.concurrent.ex5;
 
-public class Exam0110 {
+public class My {
 
   static class MyList {
     int[] values = new int[100];
     int size;
 
-    public void add(int value) {
-      // 여러 스레드가 동시에 이 메서드에 진입하면
-      // 배열의 값을 덮어쓰는 문제가 발생한다.
-      // 이렇게 여러 스레드가 동시에 접근했을 때 문제가 발생하는 코드 부분을
-      // "Critical Section" 또는 "Critical Region" 이라 부른다.
+    synchronized public void add(int value) {
       if (size >= values.length) {
         delay();
         return;
@@ -29,15 +24,14 @@ public class Exam0110 {
       }
     }
 
-    public void delay() {
-      int count = (int)(Math.random() * 1000);
+    public void delay(){
+      int count = (int) (Math.random() * 1000);
       for (int i = 0; i < count; i++) {
         Math.atan(34.1234);
       }
     }
   }
-
-  static class Worker extends Thread {
+  static class Worker extends Thread{
     MyList list;
     int value;
 
@@ -52,10 +46,10 @@ public class Exam0110 {
         list.add(value);
       }
     }
+
   }
 
   public static void main(String[] args) throws Exception {
-
     MyList list = new MyList();
 
     Worker w1 = new Worker(list, 111);
@@ -66,10 +60,9 @@ public class Exam0110 {
     w2.start();
     w3.start();
 
-    Thread.sleep(10000);
+    Thread.sleep(3000);
 
     list.print();
-
   }
 
 }
