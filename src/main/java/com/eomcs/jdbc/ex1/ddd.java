4f50b314645a3da5e89ec82efdd132b1ced57c68
박선우 <sunwoo@bagseon-uui-MacBookPro.local>
2021-03-25 23:59:25 +0900

    .

--- src/main/java/com/eomcs/jdbc/ex1/ddd.java
+++ src/main/java/com/eomcs/jdbc/ex1/ddd.java
@@ -4,4 +4,9 @@ public class ddd {
   java.sql.ResultSet rs = stmt.executeQuery(//
       "select * from x_board order by board_id desc");
 
-}
+}f (isReceived) {
+  // 서버에서 한 개 가져온 결과를 출력한다.
+  System.out.printf("%s, %s, %s, %s, %s\n", //
+      rs.getString(1), // board_id
+      rs.getString(2), // title
+      rs.getString
