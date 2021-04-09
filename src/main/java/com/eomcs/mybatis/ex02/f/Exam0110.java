a14794d3849b252628525b1586eb0854bb333cec
박선우 <sun960127@naver.com>
2021-04-09 17:18:53 +0900

    .

--- src/main/java/com/eomcs/mybatis/ex02/f/Exam0110.java
+++ src/main/java/com/eomcs/mybatis/ex02/f/Exam0110.java
@@ -1,4 +1,4 @@
-// SqlSession 사용법 - 결과가 여러 개일 때 => selectOne() 사용하면?
+// SqlSession.selectOne() 사용법 - 한 개의 결과를 조회할 때 사용
 package com.eomcs.mybatis.ex02.f;
 
 import org.apache.ibatis.io.Resources;
@@ -14,8 +14,6 @@ public class Exam0110 {
 
     // selectOne()
     // - select 결과가 0 또는 1개 일 때 호출할 수 있다.
-    // - 여러 개의 결과가 나오는 select 문에 대해 호출하면 예외가 발생한다.
-    // - 리턴 값은 한 개의 결과 레코드 값을 담은 객체이다.
     // 
     Board b = sqlSession.selectOne("BoardMapper.selectBoard1");
 
